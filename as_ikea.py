bc6580eb95a113c95f5e17cd44bb6c2058d3b1d8
Vladimir Lemberg <62575608+vladlemberg@users.noreply.github.com>
2020-10-29 12:39:42 -0700

    Update as_ikea.py
    - added more ikea letters with random selection. -)
    - proper capitalization.

--- as_ikea.py
+++ as_ikea.py
@@ -1,16 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
 """Make your last name sound as an Ikea product.
 
 As seen in https://www.facebook.com/max.abelev/posts/3347430315294332
 """
 
 def as_ikea(name: str) -> str:
-    mapper = dict(a="ä", o="ö", u="ü")
-    for letter in mapper.keys():
-       name = name.replace(letter, mapper[letter])
-    return name[::-1]
+    """
+    Convert letters to ikea letters
+    :param: string
+    :return: ikea string
+    """
+    mapper = dict(a=["à", "á", "â", "ä", "æ", "ã", "ā"],
+                  o=["ô", "ö", "ò", "ó", "œ", "ø", "ō"],
+                  u=["û", "ü", "ù", "ú", "ū"],
+                  e=["è", "é", "ê", "ë", "ē", "ė"])
 
-assert as_ikea("Ivanov") == 'Vönävi' #fails - add .lower() and .capitalize()
+    for index, letter in enumerate(name.lower(), start=0):
+        if mapper.get(letter):
+            name = replacer(name, random.choice(mapper[letter]), index)
 
+    return name[::-1].title()
+
+
+def replacer(s: str, newstring: str, index: int, nofail: bool = False) -> str:
+    """
+    String replacer at certain index
+    :param: string, new string, index
+    :return: new string
+    """
+    # raise an error if index is outside of the string
+    if not nofail and index not in range(len(s)):
+        raise ValueError("index outside given string")
+
+    # insert the new string between "slices" of the original
+    return s[:index] + newstring + s[index + 1:]
+
+
+#assert as_ikea("Ivanov") == 'Vönävi'
+
+if __name__ == "__main__":
+    print(as_ikea('Abelev'))
+    
 # Change next:
 #  - can you write a pseudocode for this procedure?
 #  - add more tests that fail
